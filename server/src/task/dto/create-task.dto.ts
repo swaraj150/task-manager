5b6dea06598887e6a6aaf7429e2d89096a2e60c0
Swaraj Andhale <swaraj@incubyte.co>
2026-04-01 19:00:37 +0530

    feat: CRUD implementation for task

--- server/src/task/dto/create-task.dto.ts
+++ server/src/task/dto/create-task.dto.ts
@@ -1,1 +1,9 @@
-export class CreateTaskDto {}
+import { TaskStatus, TaskPriority } from '../../../generated/prisma/client';
+
+export class CreateTaskDto {
+  title: string;
+  description?: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  dueDate: Date;
+}
--- server/src/task/task.service.ts
+++ server/src/task/task.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class TaskService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+    return this.prismaService.task.create({
+      data: {
+        title: createTaskDto.title,
+        description: createTaskDto.description,
+        status: createTaskDto.status,
+        priority: createTaskDto.priority,
+        dueDate: createTaskDto.dueDate,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all task`;
+    return this.prismaService.task.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  findOne(id: string) {
+    return this.prismaService.task.findUnique({ where: { id } });
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  update(id: string, updateTaskDto: UpdateTaskDto) {
+    return this.prismaService.task.update({ where: { id }, data: updateTaskDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  remove(id: string) {
+    return this.prismaService.task.delete({ where: { id } });
   }
 }
